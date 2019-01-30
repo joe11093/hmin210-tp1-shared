9ec04aeca7be451d6955f9116179cf47b85d0688
joe11093 <joe11093@users.noreply.github.com>
2019-01-30 17:32:24 +0100

    fixed a typo

--- src/Interfaces/CabinetVeterinaire.java
+++ src/Interfaces/CabinetVeterinaire.java
@@ -4,5 +4,5 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface CabinetVeterinaire extends Remote {
-	Animal searchAnimal(String nama) throws RemoteException;
+	Animal searchAnimal(String name) throws RemoteException;
 }
