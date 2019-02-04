35b8ae4b34c6818faeedd3c92ac9d452cc38b6cd
joe11093 <joe11093@users.noreply.github.com>
2019-02-04 11:50:26 +0100

    modfied the addAnimal method to take a Species instead of String

--- src/Interfaces/CabinetVeterinaire.java
+++ src/Interfaces/CabinetVeterinaire.java
@@ -5,6 +5,7 @@ import java.rmi.RemoteException;
 
 public interface CabinetVeterinaire extends Remote {
 	Animal searchAnimal(String name) throws RemoteException;
-	void addAnimal(String name, String owner, String species, String race) throws RemoteException;
+	//void addAnimal(String name, String owner, String species, String race) throws RemoteException;
+	void addAnimal(String name, String owner, Species species, String race) throws RemoteException;
 	
 }
